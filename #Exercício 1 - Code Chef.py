fea38e657f8014bfb6245cb6cf35feba90957cfb
Lucas Benzi Nucci <lucasbnucci@gmail.com>
2022-01-14 23:10:55 -0300

    Exercise 1 codechef

---
+++ #Exercício 1 - Code Chef.py
@@ -0,0 +1,14 @@
+#Exercício 1 - Code Chef
+
+withdraw_value,account_balance = input().split()
+
+withdraw_value = int(withdraw_value)
+account_balance = float(account_balance)
+
+def withdraw(withdraw_value, account_balance):
+    if ((withdraw_value % 5) == 0) and (account_balance > (withdraw_value + 0.5)):
+        print("%.2f" % (account_balance - withdraw_value - 0.5))
+    else:
+        print("%.2f" % account_balance)
+
+withdraw(withdraw_value, account_balance)
