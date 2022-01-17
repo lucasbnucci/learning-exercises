a2f023637c96a9e739e85c9c29f019f16fe59627
Lucas Benzi Nucci <lucasbnucci@gmail.com>
2022-01-16 22:15:46 -0300

    exercise LUCKFOUR answered
    Good progress today, answered this exercise faster in about 2 minutes, didn't have to correct anything on the code.

--- codechef/beginner/FLOW004.py
+++ codechef/beginner/FLOW004.py
@@ -6,4 +6,4 @@ def sum_of_first_and_last_digit(n):
         answer = int(str(number)[0]) + (number % 10) #My initial idea was something like str(number[len[number]]) but the remainder of a division by 10 looked good too. I have to learn how to measure which code is better.
         print(answer)
 
-sum_of_first_and_last_digit(n) 
+sum_of_first_and_last_digit(n)
---
+++ codechef/beginner/LUCKFOUR.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def count_four(n):
+    for i in range(0,n):
+        number = str(input())
+        answer = 0
+        for digit in number:
+            if int(digit) == 4:
+                answer += 1
+        print(answer)
+
+count_four(n)
+
+
