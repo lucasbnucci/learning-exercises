4ecb4e878e346ec3f09b92cedebe806b2259107b
Lucas Benzi Nucci <lucasbnucci@gmail.com>
2022-01-16 22:06:56 -0300

    exercise FLOW004 answered
    Just saw the necessity to evaluate which code runs faster/works better. Have to learn about that at some point to find the optimal solution.

---
+++ codechef/beginner/FLOW004.py
@@ -0,0 +1,9 @@
+n = int(input().strip())
+
+def sum_of_first_and_last_digit(n):
+    for i in range(0,n):
+        number = int(input())
+        answer = int(str(number)[0]) + (number % 10) #My initial idea was something like str(number[len[number]]) but the remainder of a division by 10 looked good too. I have to learn how to measure which code is better.
+        print(answer)
+
+sum_of_first_and_last_digit(n) 
