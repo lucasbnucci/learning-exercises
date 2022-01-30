ff406a36430daa95ff479202517d0302915572c8
Lucas Benzi Nucci <lucasbnucci@gmail.com>
2022-01-29 21:52:00 -0300

    using pyautogui to automate things, looks like a good lib

---
+++ macro.py
@@ -0,0 +1,28 @@
+import pyautogui 
+import time
+
+time.sleep(3)
+
+while True:
+    pyautogui.hotkey('1') #spurt
+    for i in range(1,420):
+        pyautogui.hotkey('e')
+        time.sleep(0.1)
+
+    time.sleep(2)
+    pyautogui.hotkey('5') #heal5
+
+    for i in range(1,50):
+        pyautogui.hotkey('e')
+        time.sleep(0.1)
+
+    time.sleep(1)
+    pyautogui.hotkey('1') #spurt
+
+    for i in range(1,420):
+        pyautogui.hotkey('e')
+        time.sleep(0.1)
+
+    time.sleep(1)
+    pyautogui.hotkey('0') #rest
+    time.sleep(60)
