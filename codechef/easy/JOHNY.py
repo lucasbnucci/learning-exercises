ddd54ea3c917a2701f90ac7eaea3144b580d9cf4
Lucas Benzi Nucci <lucasbnucci@gmail.com>
2022-02-02 22:50:46 -0300

    got

--- codechef/easy/JOHNY.py
+++ codechef/easy/JOHNY.py
@@ -2,7 +2,7 @@ t = int(input())
 
 for i in range(t):
     number_of_songs = int(input())
-    list_of_songs = list(map(int, input().split(' ')))[:number_of_songs]
+    list_of_songs = list(map(int, input().split()))[:number_of_songs]
     position_johnny = int(input())
     ans = list_of_songs[position_johnny - 1]
     list_of_songs.sort()
